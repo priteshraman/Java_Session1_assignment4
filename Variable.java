5ced5e456450216deea6778317a717d08013d02a
priteshraman <mepriteshraman@gmail.com>
2016-06-02 01:36:54 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,13 @@
+public class Variable{
+	public static void main(String args[]){
+		int x=5,y=10;
+	 System.out.println("the value of  x + y * 2 is "+ (x + y * 2));
+	 System.out.println("the value of  x - y + 2 is "+( x - y + 2));
+	 System.out.println("the value of  (x+y) * 2 is "+( (x+y) * 2));
+	 System.out.println("the value of  y % x is "+( y % x));
+		
+		
+		
+		
+	}
+}
